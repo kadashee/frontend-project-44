b05a1bf9cbf1c563c5d0429addd3cc8684c38648
kadashee <mikita.mazeika@yandex.ru>
2025-01-31 19:44:46 +0300

    Added src/cli.js for asking name function

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const sayHello = () => {
+
+    const name = readlineSync.question('May I have your name? ');
+
+    return `Hello, ${name}`;
+}
+
+export { sayHello };

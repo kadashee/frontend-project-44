a1a493523ad3bb6ed6a86684cefe8dedc7154722
kadashee <mikita.mazeika@yandex.ru>
2025-02-04 03:13:54 +0300

    Eslint config changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+
+export default {
+  languageOptions: {
+    globals: { ...globals.browser, ...globals.node },
+  },
+  ...pluginJs.configs.recommended,
+  rules: {
+  },
+};
